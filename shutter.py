72dd115788ffbe70a60ba9aa05c7b34d54dd5177
Isaac Springer <onyxhat@users.noreply.github.com>
2014-10-31 19:45:44 -0500

    Update shutter.py

--- shutter.py
+++ shutter.py
@@ -4,10 +4,8 @@
 import cv2
 import Adafruit_BBIO.GPIO as GPIO
 import time, datetime
-import os
 import subprocess
 import pysftp
-import futures
 
 ###Functions
 def get_time():
@@ -19,21 +17,13 @@ def take_picture(file):
 	cam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,1080)
 	retval, im = cam.read()
 	cv2.imwrite(file, im)
-	os.system('clear')
+	subprocess.call('clear')
 	del(cam)
-
-def upload_sftp(file, server, username, password):
-	try:
-		with pysftp.Connection(server, username=username, password=password) as sftp:
-			with sftp.cd('gallery.nuptiae.us/pictures'):
-				sftp.put(file)
-	except:
-		print "Unable to upload file."
 
 def display_image(file):
     try:
-        command = "feh %s -F -Z" % file
-        subprocess.call('killall feh', shell=True)
+        command = "fbi -a -T 1 %s" % file
+        subprocess.call('killall fbi', shell=True)
         viewImg = subprocess.Popen(command, shell=True)
     except:
         print "Unable to display %s" % file
@@ -43,28 +33,25 @@ GPIO.setup("P8_12", GPIO.IN)
 GPIO.setup("P8_10", GPIO.OUT)
 
 ###Runtime
-#os.system('ntpdate -b -s -u pool.ntp.org')
-os.system('clear')
+#subprocess.call('ntpdate -b -s -u pool.ntp.org')
+subprocess.call('clear')
 old_switch_state = 0
-executor = futures.ProcessPoolExecutor(max_workers=1)
 
 GPIO.output("P8_10", GPIO.LOW)
 
 while True:
 	new_switch_state = GPIO.input("P8_12")
 	if new_switch_state == 1 and old_switch_state == 0:
-		file = "/pictures/" + get_time() + ".png"
+		file = "/pictures/%s.png" % get_time()
 
 		for i in xrange(3, 0, -1):
 			print "%i..." % (i)
 			time.sleep(1.0)
 
-		print "CHEESE!"
-
 		GPIO.output("P8_10", GPIO.HIGH)
+		print "CHEESE!"
 		take_picture(file)
 		display_image(file)
-		#future = executor.submit(upload_sftp, file, 'ftp.sername.com', 'username', 'password')
 		GPIO.output("P8_10", GPIO.LOW)
 
 	old_switch_state = new_switch_state
